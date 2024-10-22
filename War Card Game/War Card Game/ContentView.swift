d44a80eb626d7973cc560e02eec422b96a91b600
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2024-10-22 17:33:35 -0400

    Add files via upload

---
+++ War Card Game/War Card Game/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Christian Mcfadden on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            VStack{
+                NavigationLink() {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Christian Mcfadden on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
